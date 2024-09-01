05920ab26e7e7f339ddd37cdfdb33a44ec853429
omprakash8788 <omprakashkumar552@gmail.com>
2024-09-01 23:19:03 +0530

    here i am writing the logic of add song via admin panel

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom"
-import { ToastContainer } from "react-toastify"
+// import { ToastContainer } from "react-toastify"
 import AddSong from "./pages/AddSong"
 import AddAlbum from "./pages/AddAlbum"
 import ListSong from "./pages/ListSong"
 import ListAlbum from "./pages/ListAlbum"
 import Sidebar from "./components/Sidebar"
 import Navbar from "./components/Navbar"
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const url = "http://localhost:5000"
 
 const App = () => {
   return (
