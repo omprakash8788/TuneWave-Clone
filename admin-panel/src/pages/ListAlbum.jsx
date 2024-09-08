c1f77d437ceac6f0dcd3cd5e4dd5c6f8031d576c
omprakash8788 <omprakashkumar552@gmail.com>
2024-09-08 10:43:15 +0530

    UI of ListAlbum

--- admin-panel/src/pages/ListAlbum.jsx
+++ admin-panel/src/pages/ListAlbum.jsx
@@ -23,11 +23,56 @@ const ListAlbum = () => {
     }
   }
 
+  // Delete data logic from bc and fe
+  const removeAlbum = async(id)=>{
+    try {
+      const response = await axios.post(`${url}/api/album/remove`, {id});
+      if(response.data.success){
+        toast.success(response.data.message);
+        await fetchAlbums();
+      }
+      
+    } catch (error) {
+      toast.error("Error Occured")
+      console.log(error);
+      
+      
+    }
+  }
+
   useEffect(()=>{
   fetchAlbums()
   },[])
   return (
-    <div>ListAlbum</div>
+    <div>
+      <p>All Albums List</p>
+      <br />
+      <div>
+        <div className="sm:grid hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Description</b>
+          <b>Album Colour</b>
+          <b>Action</b>
+
+        </div>
+        {
+          data.map((item, index)=>{
+            return(
+              <div key={index} className="grid grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5">
+               <img src={item.image} className="w-12" alt="" />
+               <p>{item.name}</p>
+               <p>{item.desc}</p>
+               <input type="color" value={item.bgColour} />
+               <p onClick={()=>removeAlbum(item._id)} className="cursor-pointer">X</p>
+
+
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
